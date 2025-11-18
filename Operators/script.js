c816cd2279442ffd12628cd632c24ef5fd8984a1
Dimash <dimashuktam@icloud.com>
2025-11-18 21:34:25 +0500

    Все выполнено!

---
+++ Operators/script.js
@@ -0,0 +1,121 @@
+// let sum = 5 + 3;
+// console.log(sum);
+
+
+// let text = "Hello, " + "world!";
+// console.log(text);
+
+
+// let difference = 10 - 4;
+// console.log(difference)
+
+// let quotient = 6 * 7;
+// console.log(quotient)
+
+// let bolu = 10 / 2; 
+// console.log(bolu);
+
+// let num = 6;
+// let num1 = 7;
+// console.log(num * num1);
+
+// let remainder = 18 % 5;
+// console.log(remainder);
+
+// let power = 2 ** 3;
+// console.log(power);
+
+// let number = 10;
+// number--;
+// console.log(number);
+
+// let a = 5;
+// let b = '6';
+// console.log(a !== b);
+
+// let x = 10;
+// let y = 5;
+// console.log(y <= 6);
+
+
+
+// let isTrue = true;
+// console.log(!isTrue);
+
+// let isBothTrue = (5 > 3 || 5 > 6);
+// console.log(isBothTrue);
+
+
+// let a = true;
+// let b = false;
+// console.log(a || b);
+// console.log(b || false);
+
+
+// let userName = null;
+// let defaultName = "Аноним";
+// // let name = userName ?? defaultName;
+// console.log(userName ?? defaultName);
+
+// let num = 1;
+// let num1 = 1;
+// let nam = num ?? num1;
+// console.log(nam);
+
+
+
+// ЗАДАНИЕ 1 
+
+let a = 10;
+let b = 5;
+let c = 2;
+
+console.log(a + b + c);
+console.log(a + c - b);
+console.log(a * b / c);
+console.log(a % b);
+
+// ЗАДАНИЕ 2 
+
+// let x = 7;
+
+// console.log(x++ + ++x);
+// console.log(x-- - --x);
+
+// ЗАДАНИЕ 3
+
+let num = 5;
+let num1 = 4;
+let result = num + num1;
+console.log("The result is: " + result);
+
+// Задание 4 
+
+
+
+let reslt = a > b && a > 0 && b > 0;
+let reslt1 = c < 10 || c == 2;
+let reslt2 = a / c;
+
+console.log(reslt);
+console.log(reslt1);
+console.log(reslt2 !== 5);
+
+// ЗАДАНИЕ 5
+
+let num2 = 10;
+let strNum = "10";
+
+console.log(num2 == strNum); // true, сравнивает значение с приведением типов
+console.log(num2 === strNum); // false, строгая проверка проверяет и значение, и тип данных 
+
+// ЗАДАНИЕ 6
+let x = 8;
+
+let rslt = ((x % 2 === 0 || x % 3 === 0) && x % 6 !== 0);
+console.log(rslt);
+
+let y = 3;
+console.log(!(y > 10 || y < 5));
+
+
